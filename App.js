1a7cbf6a472b6ed08c2f20264fe056d97fd21138
Johanes Indra Pradana Pao <financiallover@gmail.com>
2020-03-04 09:54:39 +0700

    Barebone development

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.selamatDatang}>Selamat datang di aplikasi barcode-scanner!</Text>
     </View>
   );
 }
@@ -12,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#393E46',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  selamatDatang: {
+    color: '#EEEEEE'
+  }
 });
